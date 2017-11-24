4df5c0f70607b76327f7b117bcf67e0173382cb6
Thanakrit Buranakarn <Guy_Krups@hotmail.com>
2017-11-24 18:16:52 +0700

    commit24/11/2560

---
+++ src/logic/Hitbox.java
@@ -0,0 +1,26 @@
+package logic;
+
+import javafx.scene.canvas.GraphicsContext;
+import sharedObject.IRenderable;
+
+public class Hitbox implements IRenderable{
+
+	@Override
+	public void draw(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getZ() {
+		// TODO Auto-generated method stub
+		return -9000;
+	}
+	
+}
